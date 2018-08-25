2c07259c13c7ea8b095427b48fa032c03dd9174e
Melissa Ho <homelissa93@gmail.com>
2018-08-25 16:08:07 -0700

    joins exercises

--- lib/07a_joins_bonus.rb
+++ lib/07a_joins_bonus.rb
@@ -26,6 +26,12 @@ require_relative './sqlzoo.rb'
 def alison_artist
   # Select the name of the artist who recorded the song 'Alison'.
   execute(<<-SQL)
+    SELECT
+      artist
+    FROM
+      albums
+    JOIN
+      tracks ON albums.asin = tracks
   SQL
 end
 
